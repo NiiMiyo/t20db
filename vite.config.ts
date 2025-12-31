3900b29ca2401b6461536c591c0403e5fb35043b
Selene Haruno <selene.haruno.ss@gmail.com>
2025-12-31 12:43:01 -0300

    Gerar JSON estático de Raças

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from "node:path";
+
+import { defineConfig } from "vite";
+import generateFile from "vite-plugin-generate-file";
+
+export default defineConfig( {
+	resolve: {
+		alias: {
+			"@": path.resolve( __dirname, "src" ),
+		},
+	},
+
+	plugins: [
+		generateFile( [
+			{
+				type: "json",
+				output: "./racas.json",
+				data: require( "./src/data/racas/index.ts" ).Racas,
+			}
+		] )
+	],
+} );
