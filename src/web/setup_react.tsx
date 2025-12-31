38332f55aa9bc928bdb418a37996986eb0e3985e
Selene Haruno <selene.haruno.ss@gmail.com>
2025-12-31 16:49:47 -0300

    Mudar para Navegação Declarativa

--- src/web/setup_react.tsx
+++ src/web/setup_react.tsx
@@ -1,25 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { PagesHref, HomePage, RacasPage } from "@/web/pages";
 
 import "@/web/styles/global.less";
 
 const root = createRoot( document.getElementById( "react-app" )! );
-root.render( <RouterProvider router={ createBrowserRouter( [
-	{
-		path: "/",
-		element: <Outlet />,
-		errorElement: <Navigate to={ PagesHref.home } />,
-		children: [
-			{
-				path: PagesHref.home,
-				element: <HomePage />
-			},
-			{
-				path: PagesHref.racas,
-				element: <RacasPage />
-			},
-		],
-	},
-] ) } /> );
+root.render( <BrowserRouter>
+	<Routes>
+		<Route Component={ HomePage } path={ PagesHref.home } />
+		<Route Component={ RacasPage } path={ PagesHref.racas } />
+	</Routes>
+</BrowserRouter> );
--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,6 @@ export default defineConfig( {
 				output: "./racas.json",
 				data: require( "./src/data/racas/index.ts" ).Racas,
 			}
-		] )
+		] ),
 	],
 } );
